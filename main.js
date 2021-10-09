49df14829fe90a5ecf59fb75dedc8540523c45b4
Diego Botero <74969346+dboterom@users.noreply.github.com>
2021-10-08 20:30:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function sayGoodLuck (){
+    alert ("Buena Suerte");
+    console.log ("Buena Suerte")
+}
+    
+function add (){
+    numberOne=Number("number-1");
+    numberTwo=Number("number-2");
+    result=Number(numberOne)+Number(numberTwo);
+    console.log=result
+}
+
+function substract (){
+    numberOne=Number("number-1");
+    numberTwo=Number("number-2");
+    result=Number(numberOne)-Number(numberTwo);
+    console.log=result
+}
+
+function multiply (){
+    numberOne=Number("number-1");
+    numberTwo=Number("number-2");
+    result=Number(numberOne)*Number(numberTwo);
+    console.log=result
+}
+
+function divide (){
+    numberOne=Number("number-1");
+    numberTwo=Number("number-2");
+    result=Number(numberOne)/Number(numberTwo);
+    console.log=result
+}
+
+function circleRadius (){
+    let ancho=500;
+    let alto=250;
+    let circleRadius=Math.min(ancho, alto)/2;
+    let x=ancho/2;
+    let y=alto/2;
+    console.log(circleRadius)
+    console.log(x);
+}
